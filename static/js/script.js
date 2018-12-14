4721051ee7733fd3ef242503f944e738b50c5d2f
Jaidev Shriram <jaidevtrumpet@gmail.com>
2018-12-14 20:04:55 +0530

    Fixed Modals

--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,39 @@
 function redirect(){
-			var a = document.getElementById("year").value;
-			if(a<1980 || a>2050 || a.length<4){
-				$('#Incorrect').modal()
-				//alert("Incorrect Input Type");
-				return false;
-			}
-			else {
-				console.log(a);
-				var b='./batch/' + a + '.html';
-				console.log(b);
-				window.location=b;
-				return true;
-		}
+	var a = document.getElementById("year").value;
+	if(a<1980 || a>2050 || a.length<4){
+		$('#Incorrect').modal();
+		//alert("Incorrect Input Type");
+		return false;
+	}
+	else {
+		console.log(a);
+		var b='./batch/' + a + '.html';
+		console.log(b);
+		window.location=b;
+		return true;
+	}
 }
 
 function modalredirect(){
-			var a = document.getElementById("year").value;
-			if(a<1980 || a>2050 || a.length<4){
-				$('#RegisterModal').modal('hide')
-				$('#BatchModal').modal('hide')
-				$('#Incorrect').modal()
-				//alert("Incorrect Input Type");
-				return false;
-			}
-			console.log(a);
-			var b='./batch/' + a + '.html';
-			console.log(b);
-			window.location=b;
-			return true;
+	var a = document.getElementById("year").value;
+	if(a<1980 || a>2050 || a.length<4){
+		$('#RegisterModal').modal('hide');
+		$('#BatchModal').modal('hide');
+		$('#Incorrect').modal();
+		//alert("Incorrect Input Type");
+		return false;
+	}
+	else {
+		console.log(a);
+		var b='./batch/' + a + '.html';
+		console.log(b);
+		window.location=b;
+		return true;
+	}	
 }
+
+$('#document').ready(function(){
+    $('#btn-modal').on('click', function(){
+        $('#year').val('');
+    });
+});
